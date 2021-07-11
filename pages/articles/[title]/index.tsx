d24aab0156c0a5faa95084024eb6abc28ebb0894
kawaPC <kawamoto.hp0@gmail.com>
2021-07-11 10:21:32 +0900

    Fix 記事ページのルーティング

---
+++ pages/articles/[title]/index.tsx
@@ -0,0 +1,50 @@
+import { getAllArticles, getArticle } from "utils/articlesUtil";
+import { serialize } from "next-mdx-remote/serialize";
+import { GetStaticPaths, GetStaticProps } from "next";
+import { MDXRemote, MDXRemoteSerializeResult } from "next-mdx-remote";
+import { IArticle } from "types/article";
+
+type Props = {
+  source: MDXRemoteSerializeResult;
+  frontMatter: Omit<IArticle, "slug">;
+};
+
+const ArticlePage: React.FC<Props> = ({ source, frontMatter }: Props) => {
+  return (
+    <article>
+      <h1>{frontMatter.title}</h1>
+
+      <MDXRemote {...source} />
+    </article>
+  );
+};
+
+export default ArticlePage;
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { content, data } = getArticle(params?.title as string);
+
+  const mdxSource = await serialize(content, { scope: data });
+
+  return {
+    props: {
+      source: mdxSource,
+      frontMatter: data,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const articles = getAllArticles(["slug"]);
+
+  const paths = articles.map((article) => ({
+    params: {
+      title: article.slug,
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
