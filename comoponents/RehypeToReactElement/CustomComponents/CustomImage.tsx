28f787963b5cf18e4a03d2487dee36003907f936
kawaPC <kawamoto.hp0@gmail.com>
2021-07-15 17:00:12 +0900

    Feature Markdown用置き換えコンポーネント

---
+++ comoponents/RehypeToReactElement/CustomComponents/CustomImage.tsx
@@ -0,0 +1,30 @@
+import Image from "next/image";
+
+type Props = {
+  src: string;
+  alt?: string;
+  title?: string;
+};
+
+const CustomImage: React.VFC<Props> = (props) => {
+  return (
+    <figure className="w-full flex justify-center flex-col mb-5">
+      <Image
+        src={props.src}
+        alt={props.alt}
+        title={props.title}
+        width="500"
+        height="350"
+        loading="lazy"
+        objectFit={"contain"}
+      />
+      {props.title && (
+        <figcaption className="text-center text-xs text-gray-600 mt-1">
+          {props.title}
+        </figcaption>
+      )}
+    </figure>
+  );
+};
+
+export { CustomImage };
---
+++ comoponents/RehypeToReactElement/CustomComponents/index.tsx
@@ -0,0 +1,9 @@
+import { CustomImage } from "./CustomImage";
+import { CustomLink } from "./CustomLink";
+import { CustomParagraph } from "./CustomParagraph";
+
+export const CustomComponents = {
+  p: CustomParagraph,
+  a: CustomLink,
+  img: CustomImage,
+};
