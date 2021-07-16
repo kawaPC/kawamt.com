b66e0fdd0d0145b340b371ddd6ff3243757a4321
kawaPC <kawamoto.hp0@gmail.com>
2021-07-16 11:42:32 +0900

    Fix AMP image style

--- components/RehypeToReactElement/CustomComponents/CustomImage.tsx
+++ components/RehypeToReactElement/CustomComponents/CustomImage.tsx
@@ -1,3 +1,6 @@
+import { useAmp } from "next/amp";
+import Image from "next/image";
+
 type Props = {
   src: string;
   alt?: string;
@@ -5,17 +8,32 @@ type Props = {
 };
 
 const CustomImage: React.VFC<Props> = (props) => {
+  const isAmp = useAmp();
   return (
     <div className="flex justify-center flex-col mb-5">
-      <div className="amp-img-fixed-container">
-        <amp-img
-          className="contain"
-          src={props.src}
-          layout="fill"
-          alt={props.alt}
-          title={props.title}
-        />
-      </div>
+      {isAmp ? (
+        <figure className="amp-img-fixed-container">
+          <amp-img
+            className="contain"
+            src={props.src}
+            layout="fill"
+            alt={props.alt}
+            title={props.title}
+          />
+        </figure>
+      ) : (
+        <figure className="flex justify-center">
+          <Image
+            src={props.src}
+            alt={props.alt}
+            title={props.title}
+            width="500"
+            height="350"
+            loading="lazy"
+            objectFit={"contain"}
+          />
+        </figure>
+      )}
       {props.title && (
         <figcaption className="text-center text-xs text-gray-600 mt-1">
           {props.title}
