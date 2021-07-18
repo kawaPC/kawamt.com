73e78c4bfa8cf9d734290802a00d7e8c39c8c950
kawaPC <kawamoto.hp0@gmail.com>
2021-07-18 11:07:16 +0900

    Fix シンタックスハイライト追加

--- components/RehypeToReactElement/index.tsx
+++ components/RehypeToReactElement/index.tsx
@@ -2,10 +2,13 @@ import { createElement } from "react";
 import unified from "unified";
 import rehypeParse from "rehype-parse";
 import rehypeReact from "rehype-react";
+// @ts-ignore
+import highlight from "rehype-highlight";
 import { CustomComponents } from "./CustomComponents";
 
 const rehypeToReactProcessor = unified()
   .use(rehypeParse, { fragment: true })
+  .use(highlight)
   .use(rehypeReact, {
     createElement: createElement,
     components: CustomComponents,
