571a3fbe1d6b8a46dbce3c9a43dfb2fc2100dc33
kawaPC <kawamoto.hp0@gmail.com>
2021-07-28 23:25:27 +0900

    Fix style

--- components/RehypeToReactElement/CustomComponents/CustomParagraph.tsx
+++ components/RehypeToReactElement/CustomComponents/CustomParagraph.tsx
@@ -1,5 +1,5 @@
 const CustomParagraph: React.FC = ({ children }) => {
-  return <div className="mt-5 leading-7 paragpraph">{children}</div>;
+  return <div className="mt-5 paragpraph">{children}</div>;
 };
 
 export { CustomParagraph };
