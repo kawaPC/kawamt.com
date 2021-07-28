cac07fa6541688024508576ff4a52ab5f19df188
kawaPC <kawamoto.hp0@gmail.com>
2021-07-28 23:25:41 +0900

    Update about

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,14 +1,35 @@
-import Head from "next/head";
+import { AppHead } from "components/AppHead";
+import { APP_ROOT } from "types/constants";
 
 const AboutPage: React.FC = () => {
   return (
-    <section>
-      <Head>
-        <title>about</title>
-      </Head>
-      <div className="h-screen flex flex-col justify-center items-center text-xl">
-        工事中です m(_ _)m
-      </div>
+    <section className="mt-10">
+      <AppHead
+        url={`${APP_ROOT}/about`}
+        title={"about"}
+        description={"私について"}
+      />
+      <h1 className="text-2xl font-bold">About</h1>
+      <p className="mt-2">
+        かつては、バイクで神社をめぐったり、風景写真を撮ってまわったりする日々を送っていました。
+        <br />
+        現在は主にプログラミングを書いて過ごしています。
+        <br />
+        ときどき発作的にバイクで遠出をしたりキャンプに出かけたりしますが、基本的にはインドアです。
+      </p>
+
+      <h2 className="text-xl font-bold mt-5">居住地の変遷</h2>
+      <p className="mt-2">広島 → 東京 → 京都 → 東京(現在)</p>
+
+      <h2 className="text-xl font-bold mt-5">連絡先</h2>
+      <amp-img
+        src="/mail_address.png"
+        layout="fixed"
+        alt="メールアドレス"
+        title="メールアドレス"
+        width="187"
+        height="17"
+      />
     </section>
   );
 };
