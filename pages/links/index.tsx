7f22c5c454a50b5268f940b3379c30aa24084417
kawaPC <kawamoto.hp0@gmail.com>
2021-07-28 23:25:58 +0900

    Update links

--- pages/links/index.tsx
+++ pages/links/index.tsx
@@ -1,13 +1,25 @@
-import Head from "next/head";
+import { AppHead } from "components/AppHead";
+import { APP_ROOT } from "types/constants";
 
 const LinksPage: React.FC = () => {
   return (
-    <section>
-      <Head>
-        <title>リンク集</title>
-      </Head>
-      <div className="h-screen flex flex-col justify-center items-center text-xl">
-        工事中です m(_ _)m
+    <section className="mt-10">
+      <AppHead
+        url={`${APP_ROOT}/links`}
+        title="リンク一覧"
+        description="リンク一覧"
+      />
+      <h1 className="text-2xl font-bold">リンク一覧</h1>
+      <div className="mt-4 space-y-4">
+        <div>
+          <a
+            className="underline text-base font-medium text-blue-600"
+            href="https://github.com/kawaPC/kawamt.com"
+          >
+            https://github.com/kawaPC/kawamt.com
+          </a>
+          <p>このサイトのソースコードです</p>
+        </div>
       </div>
     </section>
   );
