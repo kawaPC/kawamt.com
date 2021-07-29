fdc76ea9e250bb4af40d8cd76b8dfe9a059021bd
kawaPC <kawamoto.hp0@gmail.com>
2021-07-29 19:08:08 +0900

    Fix style

--- components/RehypeToReactElement/CustomComponents/CustomParagraph.tsx
+++ components/RehypeToReactElement/CustomComponents/CustomParagraph.tsx
@@ -1,5 +1,5 @@
 const CustomParagraph: React.FC = ({ children }) => {
-  return <div className="mt-5 paragpraph">{children}</div>;
+  return <div className="mt-3 paragpraph">{children}</div>;
 };
 
 export { CustomParagraph };
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,7 +3,7 @@ import type { AppProps } from "next/app";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="container mx-auto p-3 md:pt-8 md:p-5 max-w-4xl">
+    <div className="container mx-auto p-3 md:pt-8 md:pb-14 md:px-5 max-w-4xl">
       <Header />
 
       <main>
