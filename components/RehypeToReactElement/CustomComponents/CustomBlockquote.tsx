91f748cbcc6b2b1bbc6e42b215201c6a30434a4f
kawaPC <kawamoto.hp0@gmail.com>
2021-08-19 21:04:46 +0900

    Fix style

--- components/RehypeToReactElement/CustomComponents/CustomBlockquote.tsx
+++ components/RehypeToReactElement/CustomComponents/CustomBlockquote.tsx
@@ -1,6 +1,6 @@
 const CustomBlockquote: React.FC = ({ children }) => {
   return (
-    <blockquote className="mt-5 pl-4 italic border-l-4 border-gray-200">
+    <blockquote className="my-4 pl-4 italic border-l-4 border-gray-200">
       {children}
     </blockquote>
   );
