d68b5b7b4f0fcf920c885004f529fc0c2364fd94
kawamt <kawamoto.hp0@gmail.com>
2023-10-24 02:06:01 +0900

    upgrade next-js

--- components/Typography/Title.tsx
+++ components/Typography/Title.tsx
@@ -8,13 +8,7 @@ type Props = {
 const Title: React.FC<Props> = ({ href, children }) => {
   return (
     <div className="text-2xl">
-      {href ? (
-        <Link href={href}>
-          <a>{children}</a>
-        </Link>
-      ) : (
-        children
-      )}
+      {href ? <Link href={href}>{children}</Link> : children}
     </div>
   );
 };
