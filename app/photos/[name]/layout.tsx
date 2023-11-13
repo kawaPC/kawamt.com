06b9dfe68d43bc69697fe133f30e80e4f45be831
kawamt <kawamoto.hp0@gmail.com>
2023-11-14 00:10:49 +0900

    写真ページ用のレイアウトを追加

---
+++ app/photos/[name]/layout.tsx
@@ -0,0 +1,18 @@
+import "./layout.scss";
+import { Header } from "components/Header";
+
+export default function PhotoLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div className="w-full h-screen max-w-5xl flex flex-col py-5 sm:px-5 gap-y-3 md:gap-y-5 mx-auto">
+      <div className="w-full max-w-4xl mx-auto">
+        <Header />
+      </div>
+
+      <div className="flex-1 relative flex justify-center">{children}</div>
+    </div>
+  );
+}
