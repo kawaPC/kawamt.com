32a18ddd1d5d11ae0489d317ae58586e1b99517f
kawamt <kawamoto.hp0@gmail.com>
2023-11-17 19:41:53 +0900

    fix modal back button

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Dialog, DialogContent } from "components/Dialog";
-import Image from "next/image";
+import { ModalBackButton } from "components/ModalBackButton";
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
 
@@ -19,27 +19,10 @@ export const Modal: React.FC<Props> = ({ children }) => {
     [router]
   );
 
-  const onClickClose = useCallback(
-    (e: React.MouseEvent<HTMLImageElement>) => {
-      e.stopPropagation();
-      router.back();
-    },
-    [router]
-  );
-
   return (
     <Dialog defaultOpen onOpenChange={onOpenChange}>
       <DialogContent onClick={router.back}>
-        <div className="w-screen h-screen relative">
-          <Image
-            src="/cross.svg"
-            width={35}
-            height={35}
-            alt="閉じる"
-            onClick={onClickClose}
-            className="absolute z-10 top-10 sm:top-4 right-1 cursor-pointer opacity-40 bg-white hover:opacity-60 rounded-sm"
-          />
-        </div>
+        <ModalBackButton />
         {children}
       </DialogContent>
     </Dialog>
